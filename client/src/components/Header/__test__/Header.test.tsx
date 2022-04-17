75a15bf1d4d29f04800da5006ffdf431a5e6e82d
unknown <mikhailrivera12@gmail.com>
2022-04-17 09:43:19 +0200

    updated tests for header and snapshots

--- client/src/components/Header/__test__/Header.test.tsx
+++ client/src/components/Header/__test__/Header.test.tsx
@@ -5,30 +5,31 @@ import { Provider } from 'react-redux'
 import {store} from '../../../store/store'
 import { render, cleanup } from "@testing-library/react";
 import TestRenderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 
 afterEach(cleanup);
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Provider store={store}><Header /></Provider>, div)
+    ReactDOM.render(<BrowserRouter><Provider store={store}><Header /></Provider></BrowserRouter>, div)
 })
 
 it('renders the "Devices" title', () => {
-    const { getByTestId } = render(<Provider store={store}><Header /></Provider>);
+    const { getByTestId } = render(<BrowserRouter><Provider store={store}><Header /></Provider></BrowserRouter>);
     expect(getByTestId('header')).toHaveTextContent('Devices');
 })
 
 it('renders my name', () => {
-    const { getByTestId } = render(<Provider store={store}><Header /></Provider>);
+    const { getByTestId } = render(<BrowserRouter><Provider store={store}><Header /></Provider></BrowserRouter>);
     expect(getByTestId('header')).toHaveTextContent('Micky Rivera');
 })
 
 it('renders the Ubiquiti logo', () => {
-    const { getByTestId } = render(<Provider store={store}><Header /></Provider>);
+    const { getByTestId } = render(<BrowserRouter><Provider store={store}><Header /></Provider></BrowserRouter>);
     expect(getByTestId('UI-logo')).toBeInTheDocument();
 })
 
 it('matches snapshot', () => {
-    const tree = TestRenderer.create(<Provider store={store}><Header /></Provider>).toJSON();
+    const tree = TestRenderer.create(<BrowserRouter><Provider store={store}><Header /></Provider></BrowserRouter>).toJSON();
     expect(tree).toMatchSnapshot();
 })
